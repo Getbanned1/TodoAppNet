8d0a1309dd92269bdeedb6ecaac7103b3f19f086
Vorinre <161830810+Vorinre@users.noreply.github.com>
2025-06-11 16:27:25 +0300

    desing karcase

---
+++ TodoAppNet/Converters/BooleanToVisibilityInverseConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TodoAppNet.Converters
+{
+    public class BooleanToVisibilityInverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is bool boolValue && boolValue) ?
+                Visibility.Collapsed :
+                Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
